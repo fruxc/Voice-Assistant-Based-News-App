ade7980bdfb432178f3f5a62cfb49fa71953a173
fruxc <hammadansari17@gmail.com>
2020-08-18 18:48:15 +0530

    News cards and scroll function added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import NewsCards from "./components/NewsCards/NewsCards";
+import alanBtn from "@alan-ai/alan-sdk-web";
+// import useStyles from "./styles";
+import wordsToNumbers from "words-to-numbers";
 
-function App() {
+const alanKey =
+  "e8f98f6c7f8abaccd67d04c2eeba2b862e956eca572e1d8b807a3e2338fdd0dc/stage";
+const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  // const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+          if (parsedNumber > 20) {
+            alanBtn().playText("Please try that again...");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Please try that again...");
+          }
+        }
+      },
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>AI News Application</h1>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
-}
+};
 
 export default App;
